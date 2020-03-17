92ed784645c846a83007cdd1dcea6378051638bd
Bezhan Mukhidinov <bejanhtc@gmail.com>
2020-03-18 00:39:42 +0200

    panic handling implemented

--- cmd/web/sub/adapters.go
+++ cmd/web/sub/adapters.go
@@ -1,7 +1,9 @@
 package sub
 
 import (
+	"errors"
 	"net/http"
+	"runtime/debug"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -9,8 +11,24 @@ import (
 func (e *env) displayError(w http.ResponseWriter, err error, status int) {
 	w.WriteHeader(status)
 
-	err2 := e.templateFiles.ExecuteTemplate(w, "error.html", err)
+	log.Error(err)
+
+	err2 := e.templateFiles.ExecuteTemplate(w, "error.html", http.StatusText(http.StatusInternalServerError))
 	if err2 != nil {
 		log.Error(err2)
 	}
+}
+
+func (e *env) catchPanic(f http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if rec := recover(); rec != nil {
+				err := errors.New(rec.(error).Error() + "\n\n\n" + string(debug.Stack()))
+
+				e.displayError(w, err, http.StatusInternalServerError)
+			}
+		}()
+
+		f.ServeHTTP(w, r)
+	})
 }
--- cmd/web/sub/routes.go
+++ cmd/web/sub/routes.go
@@ -1,8 +1,8 @@
 package sub
 
 func (e *env) routes() {
-	e.router.HandleFunc("/", e.handleDashboard)
-	e.router.HandleFunc("/data", e.handleData)
-	e.router.HandleFunc("/download", e.handleDownload)
-	e.router.HandleFunc("/regexp", e.handleRegexp)
+	e.router.HandleFunc("/", e.catchPanic(e.handleDashboard))
+	e.router.HandleFunc("/data", e.catchPanic(e.handleData))
+	e.router.HandleFunc("/download", e.catchPanic(e.handleDownload))
+	e.router.HandleFunc("/regexp", e.catchPanic(e.handleRegexp))
 }
